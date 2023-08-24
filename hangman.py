f6217bde3fa2c0a37471d50f658b541632e72e55
Dimitris Toumazatos <di.toumazatos@gmail.com>
2023-08-24 20:10:14 +0300

    add stage, startGame, endGame functions

--- hangman.py
+++ hangman.py
@@ -1,14 +1,26 @@
 import csv
+import random
 
-file = open("words.csv", encoding='utf-8')
+file = open("words.csv", encoding='utf-8')      # read the words from a given file
 csvreader = csv.reader(file)
 words = []
 
 for i in csvreader:
     words.append(i[0])
 
+file.close()
 
-def makeMan (lives):
+givenletters = []
+foundletters = []
+lives = 5
+wordToFind = words[0]
+abc = ["a", "b", "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+
+
+def makeMan():          # draw the hangman
+    global lives
+
     if lives == 5:
         return print("____\n|  |\n|\n|\n|\n|\n|\n")
     if lives == 4:
@@ -22,7 +34,7 @@ def makeMan (lives):
     if lives == 0:
         return print("____\n|  |\n|  O\n| \|/\n|  |\n| / \ \n|\n")
         
-def intro():
+def intro():            # print intro message
     print("Welcome to my game! This is the known to all Hangman.")
     print("RULES:")
     print("1) You must find the following word by typing the correct letters.")
@@ -31,24 +43,79 @@ def intro():
     print("4) Have fun!!!\n")
 
 
+def stage():                # play each stage (round)
+    makeMan()
+    global lives
+    global givenletters
+    global foundletters
+
+    if lives == 0:                  # check if you the player is out of lives
+        return endGame(0)
+        
+    
+    lettersMissing = 0              # print the word and the letters that have been found
+    for i in wordToFind:
+        if i in foundletters:
+            print(i, end = ' ')
+        else: 
+            lettersMissing += 1
+            print("_", end = ' ')
+    print("\n")
+
+    if lettersMissing == 0:              # check  if the game has been won 
+        return endGame(1)
+    else:                                # read the next character
+        x = input("Type a letter of your choice or type 0 to exit the game: ").lower()
+        flag = False
+        while flag == False:
+            if x not in abc and x != "0":
+                x = input("Wrong input. Please type a letter of your choice or type 0 to exit the game: ").lower()
+            elif x in givenletters:
+                x = input("You have already picked this letter. Please choose another letter or type 0 to exit the game: ").lower()
+            elif x == "0":
+                return 0
+            else:
+                flag = True
+        
+        if x in wordToFind:      # the player gave a correct letter
+            foundletters.append(x)
+        else:                    # the player gave a wrong letter
+            lives -= 1
+        givenletters.append(x)
+        return 1
+
+
+def newGame():      # set a new game
+    global givenletters
+    global foundletters
+    global wordToFind
+    global lives
+
+    wordToFind = random.choice(words)            # the word the player must find
+    foundletters = [wordToFind[0]]               # letters that have been given by the player and exist in the word he\she must find
+    givenletters = []                            # letters already given by the player
+    lives = 5
+    print("New Game\n")
+
+def endGame(result):                 # end the game
+    global wordToFind
+    if result == 0:
+        print("YOU LOST!!!\nThe correct word was " + wordToFind)
+    else:
+        print("YOU WON!!!\nCONGRATULATIONS!!!")
+
+
+    # check if the player wants to play again
+    ans = input("Do you want to play again?\nPlease type y (YES) or any other character (NO): ").lower()
+    if ans == "y":
+        newGame()
+        return 1
+    else:
+        return 0
+
 #-------------------------- The actual game -----------------------------
-done = False
 intro()
-#while done == False:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-file.close()
+newGame()
+while True:
+    if stage() == 0:
+        break
